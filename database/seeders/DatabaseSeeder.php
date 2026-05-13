969dfee10f12169015e0ce2ea8c15ec05bcfc3a3
GabrielVillagran <gabrielvillagra29@proton.me>
2026-05-13 11:33:10 -0600

    modify users and routes for secure login

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,7 +17,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::updateOrCreate(
-            ['email' => 'staff@shipright.com'],
+            ['email' => 'staff@shipright.test'],
             [
                 'name' => 'ShipRight Staff',
                 'password' => Hash::make('password'),
